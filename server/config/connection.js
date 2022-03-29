54d6a9051f0675853687553529578ab139350406
Ashton B <ashtonbrugh@gmail.com>
2022-03-29 16:23:05 -0400

    populating typeDefs/resolvers

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,9 +3,7 @@ const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGOBD_URI || 'mongodb://localhost/symmetrical-journey', {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
+    useUnifiedTopology: true
 });
 
 module.exports = mongoose.connection;
---
+++ server/seeders/seeds.js
@@ -0,0 +1,29 @@
+// const faker = require('faker')
+// const { db } = require('../config/connection');
+// const { User } = require('../models');
+
+// const seed = async () => {
+//     try {
+//         await db.sync({ force: true })
+
+//         let users = [];
+
+//         for (let i = 0; i < 50; i++) {
+//             const name = faker.name.firstName()
+//             let newUser = {
+//                 username: faker.internet.userName(),
+//                 email: faker.internet.email(name).toLowerCase(),
+//                 password: faker.internet.password()
+//             }
+
+//             users.push(newUser)
+//         };
+
+//         users.forEach(async (user) => {
+//             await User.create(user);
+//         })
+//      } catch(err) {
+//          console.log(err);
+//      }
+
+//  };
