db645f10106173af460eab7f6a065ba02f70c665
Ashton B <ashtonbrugh@gmail.com>
2022-03-29 19:41:12 -0400

    adding the front end

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-text">&copy;2022 by Ashton</div>
+    </footer>
+  );
+};
+
+export default Footer;
