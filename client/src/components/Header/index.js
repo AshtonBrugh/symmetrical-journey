bdc370c175f8f300c48c6c6ac7a962ba78053d79
Ashton B <ashtonbrugh@gmail.com>
2022-04-01 11:50:30 -0400

    writing exact paths

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <header className="header">
       <div className="header-content">
+        <Link to="/">
         <h1>Ashton's New App.</h1>
+        </Link>
+
+        <nav className="">
+          <Link to= "/login">Login</Link>
+          <Link to="/signup">Signup</Link>
+        </nav>
       </div>
     </header>
   );
--- client/src/components/ThoughtList/index.js
+++ client/src/components/ThoughtList/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const ThoughtList = ({ thoughts, title }) => {
     if (!thoughts.length) {
@@ -12,15 +13,23 @@ const ThoughtList = ({ thoughts, title }) => {
                thoughts.map(thought => (
                    <div key={thought._id} className="">
                        <p className="">
+                           <Link 
+                           to={`/profile/${thought.username}`}
+                           style={{ fontWeight: 700 }}
+                           >
                            {thoughts.username}
+                           </Link>{' '}
                            thought on {thought.createdAt}
                        </p>
+
                        <div className="">
+                           <Link to={`/thought/${thought._id}`}>
                            <p>{thought.thoughtText}</p>
                            <p className="">
                                Reactions: {thought.reactionCount} || Click to {' '}
                                {thought.reactionCount ? 'see' : 'start'} the discussion!
                            </p>
+                           </Link>
                        </div>
                     </div>
                ))}
